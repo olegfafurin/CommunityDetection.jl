43eb14a0bb2cd5a16d6065f3d32e6a37e52949ad
Oleg Fafurin <olegfafurin1@gmail.com>
2023-11-21 13:35:34 +0100

    fix algo

--- src/CommunityDetection.jl
+++ src/CommunityDetection.jl
@@ -22,8 +22,8 @@ function community_detection_nback(g::AbstractGraph, k::Int)
     ϕ = real(nonbacktrack_embedding(g, k))
     if k == 1
         c = fill(1, nv(g))
-    elseif k==2
-        c = community_detection_threshold(g, ϕ[1,:])
+    elseif k == 2
+        c = community_detection_threshold(g, ϕ[1, :])
     else
         c = kmeans(ϕ, k).assignments
     end
@@ -35,8 +35,8 @@ function community_detection_threshold(g::AbstractGraph, coords::AbstractArray)
     # 0 based thresholds are highly sensitive to errors.
     c = ones(Int, nv(g))
     # idx = sortperm(λ, lt=(x,y)-> abs(x) > abs(y))[2:k] #the second eigenvector is the relevant one
-    for i=1:nv(g)
-        c[i] = coords[i] > 0 ?  1 : 2
+    for i = 1:nv(g)
+        c[i] = coords[i] > 0 ? 1 : 2
     end
     return c
 end
@@ -57,15 +57,15 @@ See `Nonbacktracking` for details.
 """
 function nonbacktrack_embedding(g::AbstractGraph, k::Int)
     B = Nonbacktracking(g)
-    λ, eigv = Graphs.LinAlg.eigs(B, nev=k+1, which=LR())
-    ϕ = zeros(ComplexF32, nv(g), k-1)
+    λ, eigv = Graphs.LinAlg.eigs(B, nev=k + 1, which=LR())
+    ϕ = zeros(ComplexF32, nv(g), k - 1)
     # TODO decide what to do with the stationary distribution ϕ[:,1]
     # this code just throws it away in favor of eigv[:,2:k+1].
     # we might also use the degree distribution to scale these vectors as is
     # common with the laplacian/adjacency methods.
-    for n=1:k-1
-        v= eigv[:,n+1]
-        ϕ[:,n] = contract(B, v)
+    for n = 1:k-1
+        v = eigv[:, n+1]
+        ϕ[:, n] = contract(B, v)
     end
     return ϕ'
 end
@@ -90,7 +90,7 @@ function community_detection_bethe(g::AbstractGraph, k::Int=-1; kmax::Int=15)
     D = Diagonal(degree(g))
     r = (sum(degree(g)) / nv(g))^0.5
 
-    Hr = Matrix((r^2-1)*I, nv(g), nv(g)) - r*A + D;
+    Hr = Matrix((r^2 - 1) * I, nv(g), nv(g)) - r * A + D
     #Hmr = Matrix((r^2-1)*I, nv(g), nv(g)) + r*A + D;
     k >= 1 && (kmax = k)
     λ, eigv = Graphs.LinAlg.eigs(Hr, which=SR(), nev=min(kmax, nv(g)))
@@ -103,7 +103,7 @@ function community_detection_bethe(g::AbstractGraph, k::Int=-1; kmax::Int=15)
                     some communities could be meaningless. Try to reduce `k`.")
     k < 1 && (k = q)
     k <= 1 && return fill(1, nv(g))
-    labels = kmeans(collect(transpose(eigv[:,2:k])), k).assignments
+    labels = kmeans(collect(transpose(eigv[:, 2:k])), k).assignments
     return labels
 end
 
@@ -112,32 +112,34 @@ function community_detection_greedy_modularity(g::AbstractGraph)
     n = length(vertices(g))
     c = Vector(1:n)
     cs = Vector()
-    qs = fill(-1., n)
+    qs = fill(-1.0, n)
     Q, e, a = compute_modularity(g, c)
+    println(Q)
     push!(cs, c)
     qs[1] = Q
-    for i=1:n-1
+    for i = 1:n-1
         Q = modularity_greedy_step!(g, Q, e, a, c)
-        push!(cs, c)
+        println(Q)
+        push!(cs, copy(c))
         qs[i+1] = Q
     end
     imax = argmax(qs)
     return rewrite_class_ids(cs[imax])
 end
 
-function modularity_greedy_step!(g::AbstractGraph, Q::Float64, e::Matrix{<:AbstractFloat}, a::AbstractVector{<:AbstractFloat},  c::AbstractVector{<:Integer})
+function modularity_greedy_step!(g::AbstractGraph, Q::Float64, e::Matrix{<:AbstractFloat}, a::AbstractVector{<:AbstractFloat}, c::AbstractVector{<:Integer})
     m = 2 * length(edges(g))
     n = length(vertices(g))
     dq_max = -1
-    tried = Set{Tuple{Int64, Int64}}()
-    to_merge::Tuple{Integer, Integer} = (0,0)
+    tried = Set{Tuple{Int64,Int64}}()
+    to_merge::Tuple{Integer,Integer} = (0, 0)
     tried = Set()
     for edge in edges(g)
         u = min(src(edge), dst(edge))
         v = max(src(edge), dst(edge))
-        if !((u, v) in tried)
-            push!(tried, (u,v))
-            dq = 2* (e[u,v] / m - a[u]*a[v] / m^2)
+        if c[u] != c[v] && !((c[u], c[v]) in tried)
+            push!(tried, (c[u], c[v]))
+            dq = (e[c[u], c[v]] / m - a[c[u]] * a[c[v]] / m^2)
             if dq > dq_max
                 dq_max = dq
                 to_merge = (c[u], c[v])
@@ -145,22 +147,23 @@ function modularity_greedy_step!(g::AbstractGraph, Q::Float64, e::Matrix{<:Abstr
         end
     end
     c1, c2 = to_merge
-    for i=1:n
+    println(dq_max, " ", c1, " ", c2)
+    for i = 1:n
         e[c1, i] += e[c2, i]
     end
-    for i=1:n
+    for i = 1:n
         if i == c2
             continue
         end
         e[i, c1] += e[i, c2]
     end
     a[c1] = a[c1] + a[c2]
-    for i=1:n
+    for i = 1:n
         if c[i] == c2
             c[i] = c1
         end
     end
-    return Q
+    return Q + 2 * dq_max
 end
 
 
@@ -174,13 +177,13 @@ function compute_modularity(g::AbstractGraph, c::AbstractVector{<:Integer})
         for v in neighbors(g, u)
             if c[u] == c[v]
                 Q += 1
-                e[c[i], c[j]] += 1
             end
+            e[c[u], c[v]] += 1
             a[c[u]] += 1
         end
     end
     Q *= m
-    for i=1:n_groups
+    for i = 1:n_groups
         Q -= a[i]^2
     end
     Q /= m^2
@@ -190,7 +193,7 @@ end
 function rewrite_class_ids(v::AbstractVector{<:Integer})
     d = Dict()
     vn = zeros(Int64, length(v))
-    for i=1:length(v)
+    for i = eachindex(v)
         if !(v[i] in keys(d))
             d[v[i]] = length(d) + 1
         end
